65009800b30f5faa3973bb9f0156e468b23c7323
tmdwo <tmdwo99@gmail.com>
2019-12-19 17:20:06 +0900

    1

---
+++ MyBoard2/src/main/java/org/dsu/dc/service/BoardServiceImpl.java
@@ -0,0 +1,53 @@
+package org.dsu.dc.service;
+
+import java.util.List;
+
+import org.dsu.dc.domain.BoardVO;
+import org.dsu.dc.domain.Criteria;
+import org.dsu.dc.mapper.BoardMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class BoardServiceImpl implements BoardService{
+	@Override
+	public int getTotal(Criteria cri) {
+		log.info("[Board Get Total Count ....]");
+		return mapper.getTotalCount(cri);
+	}
+	@Autowired
+	private BoardMapper mapper;
+	
+	@Override
+	public List<BoardVO> list() throws Exception {
+		return mapper.getList();
+	}
+	@Override
+	public List<BoardVO> getList(Criteria cri){
+		log.info("[Board Get List with Criteria ....]{}, cri");
+		return mapper.getListWithPaging(cri);
+	}
+	@Override
+	public void register(BoardVO board) {
+		log.info("[Board Register....] {}", board);
+		mapper.insertSelectKey(board);
+	}
+	@Override
+	public BoardVO get(Long bno) {
+		log.info("[Board Get ....] BNO={}",bno);
+		return mapper.read(bno);
+	}
+	@Override
+	public boolean modify(BoardVO board) {
+		log.info("[Board Modify ....] {}", board);
+		return mapper.update(board) == 1;
+	}
+	@Override
+	public boolean remove(Long bno) {
+		log.info("[Board Remove ....] bno={}", bno);
+		return mapper.delete(bno) == 1;
+	}
+}
